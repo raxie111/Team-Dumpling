12e3914bd10cd75c4597583e4c3301fb37f32ce2
Yuzhao3 <yuzhao3@s.sfusd.edu>
2023-03-07 15:45:37 -0800

    Mute Button

--- Assets/Scripts/MuteAudio.cs
+++ Assets/Scripts/MuteAudio.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mute : MonoBehaviour
+public class MuteAudio : MonoBehaviour
 {
     public void MuteToggle(bool muted)
     {
