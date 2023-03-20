b59e05e9bb3ee9de037d46fc0981052b872a6098
Yuzhao3 <yuzhao3@s.sfusd.edu>
2023-03-20 15:46:47 -0700

    Button Update

---
+++ Assets/Scripts/GamePause.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GamePause : MonoBehaviour
+{
+   [SerializeField] public GameObject PauseGame;
+
+
+    public void PlayScene()
+    {
+        PauseGame.SetActive(false);
+        Time.timeScale = 1;
+
+    }
+
+    public void PauseScene()
+    {
+        PauseGame.SetActive(true);
+        Time.timeScale = 0;
+    }
+}
