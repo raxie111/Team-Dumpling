d0f9963755e29eb7ad2ddb6b48101d2f9c37bbda
Aaron Huang <yohuang5@s.sfusd.edu>
2023-03-20 15:27:14 -0700

    fixed button

--- Assets/Menu/Scripts/button.cs
+++ Assets/Menu/Scripts/button.cs
@@ -22,6 +22,4 @@ public class button : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
         _img.sprite = _default;
         _source.PlayOneShot(_uncompressedClip);
     }
-
-    public void whenButtonClicked()
 }
