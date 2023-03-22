f4afb99849d7ddd8d20dd8322bc6df0be4b50f0a
Yuzhao3 <yuzhao3@s.sfusd.edu>
2023-03-22 13:10:23 -0700

    Menu + UI
    Added Muni Ui changes and removed volume button (replaced with slider)

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+
+   public void SetFullScreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
