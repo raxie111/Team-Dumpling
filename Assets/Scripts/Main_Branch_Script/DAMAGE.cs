9a78854268dbeb807fea19771866ce97ef419a52
Aaron Huang <yohuang5@s.sfusd.edu>
2023-04-04 15:27:51 -0700

    Revert "health bar added but cant kill slime"
    This reverts commit 06ab3e04b940d8cd30d7d9e7947086794d65978f.

--- Assets/Scripts/Main_Branch_Script/DAMAGE.cs
+++ Assets/Scripts/Main_Branch_Script/DAMAGE.cs
@@ -6,7 +6,18 @@ public class DAMAGE : MonoBehaviour
 {
     public PlayerController playerController;
     public int damage = 1;
-   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player")
