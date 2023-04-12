a7020406e1e3d08c581cba6f6c9073be22cfce14
Aaron Huang <yohuang5@s.sfusd.edu>
2023-04-12 08:56:42 -0700

    added meat

---
+++ Assets/Scripts/Load.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Load : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(DelaySceneLoad());
+    }
+    IEnumerator DelaySceneLoad()
+    {
+        yield return new WaitForSeconds(7f);
+        SceneManager.LoadScene("GameScene");
+    }
+}
