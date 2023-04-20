ace0fb7b30033f414060185c1b291c2c5b898bb3
Aaron Huang <yohuang5@s.sfusd.edu>
2023-04-20 15:42:17 -0700

    jj

--- Assets/Scripts/Main_Branch_Script/DAMAGE.cs
+++ Assets/Scripts/Main_Branch_Script/DAMAGE.cs
@@ -6,19 +6,8 @@ public class DAMAGE : MonoBehaviour
 {
     public PlayerController playerController;
     public int damage = 1;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
