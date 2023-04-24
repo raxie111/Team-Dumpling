e9d9b6e1a4ef1daebdefd6a1a43ffba59a9fe279
raymondx123 <56202256+raymondx123@users.noreply.github.com>
2023-04-24 15:38:54 -0700

    Revert "jj"
    This reverts commit ace0fb7b30033f414060185c1b291c2c5b898bb3.

--- Assets/Scripts/Main_Branch_Script/DAMAGE.cs
+++ Assets/Scripts/Main_Branch_Script/DAMAGE.cs
@@ -6,8 +6,19 @@ public class DAMAGE : MonoBehaviour
 {
     public PlayerController playerController;
     public int damage = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
